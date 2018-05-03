c3e7b2086c5782a1acd2e968a263f7f9743b2f0c
SamPratik <pratik.anwar@gmail.com>
2018-05-04 01:09:54 +0600

    Initial Commit

---
+++ resources/views/phonebook.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Phonebook App</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  </head>
+  <body>
+
+    <div id="app">
+
+    </div>
+
+    <script src="{{asset('js/app.js')}}"></script>
+  </body>
+</html>
